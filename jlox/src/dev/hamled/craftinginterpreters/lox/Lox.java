6d9ecee303f3bfa01d1ec11be9eac2015195157b
Charles Ellis <hamled@hamled.name>
2019-09-15 16:07:49 -0700

    Error reporting basics

--- jlox/src/dev/hamled/craftinginterpreters/lox/Lox.java
+++ jlox/src/dev/hamled/craftinginterpreters/lox/Lox.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    static boolean hadError = false;
 
     public static void main(String[] args) throws IOException {
         if(args.length > 1) {
@@ -24,6 +25,8 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        if(hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
@@ -33,6 +36,7 @@ public class Lox {
         for(;;) {
             System.out.print("> ");
             run(reader.readLine());
+            hadError = false;
         }
     }
 
@@ -44,5 +48,14 @@ public class Lox {
         for(Token token : tokens) {
             System.out.println(token);
         }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error " + where + ": " + message);
+        hadError = true;
     }
 }
