ed6f2aab9dd8118a3eb45c1d5e2a1163b94d5b94
Charles Ellis <hamled@hamled.name>
2019-09-18 19:30:37 -0700

    Scanning for one/two-char tokens

--- jlox/src/dev/hamled/craftinginterpreters/lox/Scanner.java
+++ jlox/src/dev/hamled/craftinginterpreters/lox/Scanner.java
@@ -45,9 +45,23 @@ class Scanner {
             case ';': addToken(TokenType.SEMICOLON); break;
             case '*': addToken(TokenType.STAR); break;
 
+            // One or two character tokens
+            case '!': addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG); break;
+            case '=': addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
+            case '<': addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS); break;
+            case '>': addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+
             default:
                 Lox.error(line, "Unexpected character.");
         }
+    }
+
+    private boolean match(char expected) {
+        if(isAtEnd()) return false;
+        if(source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
     }
 
     private char advance() {
