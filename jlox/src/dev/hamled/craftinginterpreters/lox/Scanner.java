8469a3bfd1aa4b0e7f171cfd84673297d16c0c94
Charles Ellis <hamled@hamled.name>
2019-09-20 20:58:22 -0700

    Scanner support for number literals

--- jlox/src/dev/hamled/craftinginterpreters/lox/Scanner.java
+++ jlox/src/dev/hamled/craftinginterpreters/lox/Scanner.java
@@ -72,8 +72,25 @@ class Scanner {
             case '"': string(); break;
 
             default:
-                Lox.error(line, "Unexpected character.");
+                if(isDigit(c)) {
+                    number();
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
         }
+    }
+
+    private void number() {
+        while(isDigit(peek())) advance();
+
+        // Look for a fractional part.
+        if(peek() == '.' && isDigit(peekNext())) {
+            advance();
+
+            while(isDigit(peek())) advance();
+        }
+
+        addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
     }
 
     private void string() {
@@ -107,6 +124,15 @@ class Scanner {
     private char peek() {
         if(isAtEnd()) return '\0';
         return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if(current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
     }
 
     private char advance() {
