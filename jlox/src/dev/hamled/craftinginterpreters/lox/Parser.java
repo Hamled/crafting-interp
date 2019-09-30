e5646e28470bb9957ebb05238288d70cb83f3416
Charles Ellis <hamled@hamled.name>
2019-09-30 13:34:24 -0700

    Parser - class outline

---
+++ jlox/src/dev/hamled/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,12 @@
+package dev.hamled.craftinginterpreters.lox;
+
+import java.util.List;
+
+class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+}
