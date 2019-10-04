da67e918674909607454708cdf2e67e28a444cdb
Charles Ellis <hamled@hamled.name>
2019-10-04 15:45:48 -0700

    Parser desugars for statement to while loop

--- jlox/src/dev/hamled/craftinginterpreters/lox/Parser.java
+++ jlox/src/dev/hamled/craftinginterpreters/lox/Parser.java
@@ -1,6 +1,7 @@
 package dev.hamled.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Parser {
@@ -38,12 +39,54 @@ class Parser {
     }
 
     private Stmt statement() {
+        if(match(TokenType.FOR)) return forStatement();
         if(match(TokenType.IF)) return ifStatement();
         if(match(TokenType.PRINT)) return printStatement();
         if(match(TokenType.WHILE)) return whileStatement();
         if(match(TokenType.LEFT_BRACE)) return new Stmt.Block(block());
 
         return expressionStatement();
+    }
+
+    private Stmt forStatement() {
+        consume(TokenType.LEFT_PAREN, "Expected '(' after 'for'.");
+
+        Stmt initializer;
+        if(match(TokenType.SEMICOLON)) {
+            initializer = null;
+        } else if(match(TokenType.VAR)) {
+            initializer = varDeclaration();
+        } else {
+            initializer = expressionStatement();
+        }
+
+        Expr condition = null;
+        if(!check(TokenType.SEMICOLON)) {
+            condition = expression();
+        }
+        consume(TokenType.SEMICOLON, "Expected ';' after loop condition.");
+
+        Expr increment = null;
+        if(!check(TokenType.RIGHT_PAREN)) {
+            increment = expression();
+        }
+        consume(TokenType.RIGHT_PAREN, "Expected ';' after for clauses.");
+        Stmt body = statement();
+
+        if(increment != null) {
+            body = new Stmt.Block(Arrays.asList(
+                    body,
+                    new Stmt.Expression(increment)));
+        }
+
+        if(condition == null) condition = new Expr.Literal(true);
+        body = new Stmt.While(condition, body);
+
+        if(initializer != null) {
+            body = new Stmt.Block(Arrays.asList(initializer, body));
+        }
+
+        return body;
     }
 
     private Stmt ifStatement() {
