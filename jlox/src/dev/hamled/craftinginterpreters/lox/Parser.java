0d80600e837df6e71ed235c036101ee76c55f5a6
Charles Ellis <hamled@hamled.name>
2019-10-03 22:13:31 -0700

    Parser supports block statements

--- jlox/src/dev/hamled/craftinginterpreters/lox/Parser.java
+++ jlox/src/dev/hamled/craftinginterpreters/lox/Parser.java
@@ -39,6 +39,7 @@ class Parser {
 
     private Stmt statement() {
         if(match(TokenType.PRINT)) return printStatement();
+        if(match(TokenType.LEFT_BRACE)) return new Stmt.Block(block());
 
         return expressionStatement();
     }
@@ -66,6 +67,17 @@ class Parser {
         consume(TokenType.SEMICOLON, "Expected ';' after expression.");
 
         return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+
+        while(!check(TokenType.RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(TokenType.RIGHT_BRACE, "Expected '}' after block.");
+        return statements;
     }
 
     private Expr assignment() {
