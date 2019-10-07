d78c54148bde7a9b87e82405c40d067045563b45
Charles Ellis <hamled@hamled.name>
2019-10-07 16:24:55 -0700

    Function object class

---
+++ jlox/src/dev/hamled/craftinginterpreters/lox/LoxFunction.java
@@ -0,0 +1,32 @@
+package dev.hamled.craftinginterpreters.lox;
+
+import java.util.List;
+
+class LoxFunction implements LoxCallable {
+    private final Stmt.Function declaration;
+    LoxFunction(Stmt.Function declaration) {
+        this.declaration = declaration;
+    }
+
+    @Override
+    public String toString() {
+        return "<fn " + declaration.name.lexeme + ">";
+    }
+
+    @Override
+    public int arity() {
+        return declaration.params.size();
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        // Setup a new environment from passed argument values
+        Environment environment = new Environment(interpreter.globals);
+        for(int i = 0; i < declaration.params.size(); i++) {
+            environment.define(declaration.params.get(i), arguments.get(i));
+        }
+
+        interpreter.executeBlock(declaration.body, environment);
+        return null;
+    }
+}
