383d75f4f4f3231a10d0218973b4addb670df68f
Charles Ellis <hamled@hamled.name>
2019-10-09 10:03:09 -0700

    Resolver supports set expression

--- jlox/src/dev/hamled/craftinginterpreters/lox/Resolver.java
+++ jlox/src/dev/hamled/craftinginterpreters/lox/Resolver.java
@@ -154,6 +154,13 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Void visitSetExpr(Expr.Set expr) {
+        resolve(expr.value);
+        resolve(expr.object);
+        return null;
+    }
+
+    @Override
     public Void visitTernaryExpr(Expr.Ternary expr) {
         resolve(expr.condition);
         resolve(expr.thenExpr);
