7231299bbd105384265136b7a08d10014dfef689
Charles Ellis <hamled@hamled.name>
2019-10-11 13:01:32 -0700

    AST node for super expression

--- jlox/src/dev/hamled/craftinginterpreters/lox/Expr.java
+++ jlox/src/dev/hamled/craftinginterpreters/lox/Expr.java
@@ -12,6 +12,7 @@ abstract class Expr {
     R visitLiteralExpr(Literal expr);
     R visitLogicalExpr(Logical expr);
     R visitSetExpr(Set expr);
+    R visitSuperExpr(Super expr);
     R visitTernaryExpr(Ternary expr);
     R visitThisExpr(This expr);
     R visitUnaryExpr(Unary expr);
@@ -116,6 +117,18 @@ abstract class Expr {
     final Expr object;
     final Token name;
     final Expr value;
+  }
+  static class Super extends Expr {
+    Super(Token keyword, Token method) {
+        this.keyword = keyword;
+        this.method = method;
+    }
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitSuperExpr(this);
+    }
+
+    final Token keyword;
+    final Token method;
   }
   static class Ternary extends Expr {
     Ternary(Expr condition, Expr thenExpr, Expr elseExpr) {
