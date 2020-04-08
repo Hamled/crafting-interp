6e7486fc291589c2e530f22561d425c335216e02
Charles Ellis <hamled@hamled.dev>
2020-04-08 08:19:50 +0700

    Clox creates and disassembles a simple Chunk
    This will suffice as our "Hello World" for the Clox interpreter.

--- clox/main.c
+++ clox/main.c
@@ -1,7 +1,17 @@
 #include <stdlib.h>
 
 #include "common.h"
+#include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    writeChunk(&chunk, 123);
+
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
+
     return EXIT_SUCCESS;
 }
