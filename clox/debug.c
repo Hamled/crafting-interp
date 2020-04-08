a321fc989d50e6089d00d43f62235613ca25364e
Charles Ellis <hamled@hamled.dev>
2020-04-08 09:52:19 +0700

    OP_CONSTANT instruction
    As with OP_RETURN, there is no execution implementation yet but we do
    have support for it in our disassembler code.

--- clox/debug.c
+++ clox/debug.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "debug.h"
+#include "value.h"
 
 void disassembleChunk(Chunk *chunk, const char *name) {
     printf("== %s ==\n", name);
@@ -15,11 +16,23 @@ static size_t simpleInstruction(const char *name, size_t offset) {
     return offset + 1;
 }
 
+static size_t constantInstruction(const char *name, Chunk *chunk,
+                                  size_t offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s 0x%02hhX '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    puts("'");
+
+    return offset + 2;
+}
+
 size_t disassembleInstruction(Chunk *chunk, size_t offset) {
     printf("%04zd ", offset);
 
     OpCode opcode = (OpCode)chunk->code[offset];
     switch(opcode) {
+        case OP_CONSTANT:
+            return constantInstruction("OP_CONSTANT", chunk, offset);
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
         default:
