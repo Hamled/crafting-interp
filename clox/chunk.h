fe74207b30ea0fea7b7f8c25e74ee32fa5470bd8
Charles Ellis <hamled@hamled.dev>
2020-04-08 09:20:17 +0700

    Chunks have a ValueArray for constants

--- clox/chunk.h
+++ clox/chunk.h
@@ -2,6 +2,7 @@
 #define __CLOX_CHUNK_H_
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
     OP_RETURN,
@@ -11,10 +12,12 @@ typedef struct {
     size_t count;
     size_t capacity;
     uint8_t *code;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk *chunk);
 void freeChunk(Chunk *chunk);
 void writeChunk(Chunk *chunk, uint8_t byte);
+size_t addConstant(Chunk *chunk, Value value);
 
 #endif // __CLOX_CHUNK_H_
